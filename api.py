81e914d7cb9030cd15f9ecdf766345fec9fa4e5c
matiasmiodosky <matias.miodosky@emilabs.ai>
2022-01-09 15:32:43 -0300

    updated host and port

--- api.py
+++ api.py
@@ -14,4 +14,4 @@ api.add_resource(helloController.HelloController, '/api/hello')
 api.add_resource(processorController.ProcessorController, '/contents')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80)
--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
-sudo yum -y update
-sudo yum -y install git
+#!/bin/bash
+
+yum -y update
+yum -y install git
 git clone https://github.com/Hawk-Eye-2021/content-processor.git
 cd content-processor
 
 # add swap
-sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=8192
-sudo chmod 600 /var/swap.1
-sudo /sbin/mkswap /var/swap.1
-sudo /sbin/swapon /var/swap.1
+dd if=/dev/zero of=/var/swap.1 bs=1M count=8192
+chmod 600 /var/swap.1
+mkswap /var/swap.1
+swapon /var/swap.1
 
 
 pip3 install -r requirements.txt --no-cache-dir
+
+python3 api.py
